99232559cb3a1eb267bc048d2c5831269a6a0f3d
Lily Pham <lilypham@lilys-macbook-pro.local>
2019-01-17 18:56:45 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Lily Pham on 1/17/19.
+//  Copyright © 2019 Lily Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
